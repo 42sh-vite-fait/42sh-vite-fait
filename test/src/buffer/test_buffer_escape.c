11065b1d543d096cfc12a13919043c1d132874ce
Darius Kramer <dev@dkramer.fastmail.com>
2016-10-13 11:24:45 +0200

    [History][Test]: Add test for newline case in buffer_escape

--- test/src/buffer/test_buffer_escape.c
+++ test/src/buffer/test_buffer_escape.c
@@ -33,11 +33,23 @@ static void		test_02_buffer_unescape_no_match(void)
 	VTS;
 }
 
+static void		test_03_buffer_escape_Newline(void)
+{
+	t_buffer	*str;
+
+	str = buffer_dup("Hello World\nAnd good morning!\n");
+	str = buffer_escape_chars(str, '\n');
+	v_assert_str("Hello World\\\nAnd good morning!\\\n", str->str);
+
+	VTS;
+}
+
 void			suite_buffer_escape(void)
 {
 	test_00_buffer_escape();
 	test_01_buffer_unescape();
 	test_02_buffer_unescape_no_match();
+	test_03_buffer_escape_Newline();
 
 	VSS;
 }
