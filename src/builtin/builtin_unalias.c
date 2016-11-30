09fafa3e48279bd54075f022f920161487b9ed49
Clément Renault <renault.cle@gmail.com>
2016-11-30 14:52:26 +0100

    [Builtin][Alias]: Add better error messages

--- src/builtin/builtin_unalias.c
+++ src/builtin/builtin_unalias.c
@@ -12,28 +12,25 @@ uint8_t		builtin_unalias(int argc, char * const *argv)
 
 	if (argc == 1)
 	{
-		ft_dprintf(2, "%s: usage: %s [-a] name [name ...]", argv[0]);
+		ft_dprintf(2, "unalias: usage: unalias [-a] name [name ...]");
 		return (2);
 	}
-	else if (argc >= 2)
+	OPT_INIT(opt);
+	if (ft_getopt(argc, argv, "a", &opt) == 'a')
 	{
-		if (ft_getopt(argc, argv, "a", &opt) == 'a')
-		{
-			alias_clear();
-			return (0);
-		}
-		ret = 0;
-		i = 1;
-		while (i < argc)
-		{
-			if (alias_unset(argv[i]) != 0)
-			{
-				ret = 1;
-				ft_dprintf(2, "%s: %s: not found\n", argv[0], argv[i]);
-			}
-			++i;
-		}
-		return (ret);
+		alias_clear();
+		return (0);
 	}
-	return (1);
+	ret = 0;
+	i = 1;
+	while (i < argc)
+	{
+		if (alias_unset(argv[i]) != 0)
+		{
+			ret = 1;
+			ft_dprintf(2, "unalias: %s: not found\n", argv[i]);
+		}
+		++i;
+	}
+	return (ret);
 }
