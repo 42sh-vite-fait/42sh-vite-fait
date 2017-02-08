7eceb1896a1a069ba15fd1cafeb6a1196a2e37e2
Darius Kramer <dev@dkramer.fastmail.com>
2017-02-08 17:15:44 +0100

    [Crash]: add script to crash the shell with /dev/random

---
+++ crash/make_crash.sh
@@ -0,0 +1,13 @@
+filename='crash_file'
+random_size=1024
+ret=0
+i=0
+
+while test ${ret} -eq 0;
+do
+	echo ${i}
+	cat /dev/random | head -c ${random_size} > ${filename}
+	cat ${filename} | ./42sh
+	ret=$?
+	i=$(( ${i} + 1 ))
+done
