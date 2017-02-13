c699f8cf0f05dcb559d5f440509787b4755652a7
Darius Kramer <dev@dkramer.fastmail.com>
2017-02-13 19:17:39 +0100

    [Crash]: add new crashes

--- crash/make_crash.sh
+++ crash/make_crash.sh
@@ -1,13 +1,18 @@
-filename='crash_file'
+tmp_name='temporary_dump'
+dump_name='dump'
 random_size=1024
 ret=0
 i=0
+SIG_LIMIT=128
 
-while test ${ret} -eq 0;
+while true;
 do
 	echo ${i}
-	cat /dev/random | head -c ${random_size} > ${filename}
-	cat ${filename} | ./42sh -i
-	ret=$?
+	cat /dev/random | head -c ${random_size} > ${tmp_name}
+	cat ${tmp_name} | ./42sh
+	if test $? -gt ${SIG_LIMIT};
+	then
+		mv ${tmp_name} "${dump_name}_${i}"
+	fi
 	i=$(( ${i} + 1 ))
 done
