4bc0dd517bf18aa7e55db5c3f905969f6eb9504a
Darius Kramer <dev@dkramer.fastmail.com>
2017-02-13 18:54:22 +0100

    [Parser]: do not print the AST if an error appeared

--- src/shell/shell_parser.c
+++ src/shell/shell_parser.c
@@ -12,7 +12,7 @@ int shell_parser(const t_string *input, t_parser *parser, const t_array *tokens)
 	ret = parser_parse(parser);
 	if (ret != PARSER_NO_ERROR)
 		error_print("parser");
-	if (opt_is_set(OPT_DEBUG_AST))
+	else if (opt_is_set(OPT_DEBUG_AST))
 		ast_debug_print(&parser->ast, input->str);
 	return (ret);
 }
