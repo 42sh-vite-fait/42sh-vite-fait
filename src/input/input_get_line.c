42sh-vite-fait/42sh-vite-fait

commit 0d2e836eeedcd1cb3e1972dcfb7046889780b2fe

[Input]: make E_INPUT_EOF mean EOF

src/input/input_get_line.c
@@ -11,11 +11,14 @@ static int	input_arg_get_line(t_string *input)
 
 	arg = opt_get_command_line();
 	assert(arg != NULL);
-	if (arg[index] == '\0')
+	if (arg[index] == '\0' && ft_strlen(arg) == 0)
 		return (E_INPUT_EOF);
 	offset = ft_strchrpos(arg + index, '\n');
 	if (offset == -1)
+	{
 		fatal_malloc(string_cat(input, arg + index));
+		index += ft_strlen(arg);
+	}
 	else
 	{
 		offset += 1;
@@ -41,7 +44,7 @@ static int	input_notty_get_line(t_string *input)
 		error_set_context("read: %s", strerror(errno));
 		return (E_INPUT_ERROR);
 	}
-	else if (ret == 0)
+	else if (ret == 0 && input->len == 0)
 		return (E_INPUT_EOF);
 	else
 		return (E_INPUT_OK);

src/shell/shell_input.c
@@ -15,7 +15,8 @@ int	shell_input(t_string *input, const char *prompt)
 		return (status);
 	while (remove_trailing_escaped_newline(input) != LINE_COMPLETE)
 	{
-		if ((status = input_get_line(&more_input, SHELL_PS2)) == E_INPUT_OK)
+		status = input_get_line(&more_input, SHELL_PS2);
+		if (status == E_INPUT_OK)
 			string_append(input, &more_input);
 		else
 			return (status);

src/shell/shell_loop.c
@@ -51,7 +51,7 @@ static int	non_interactive(t_string *input, t_array *tokens, t_parser *parser)
 	input_status = shell_input(input, SHELL_PS1);
 	if (input_status == E_INPUT_ERROR)
 		return (EXIT_FAILURE);
-	if (input_status == E_INPUT_EOF && input->len == 0)
+	if (input_status == E_INPUT_EOF)
 		return (EXIT_SUCCESS);
 
 	if (shell_lexer(input, tokens) == LEXER_ERROR)

commit 8f2bc3956b371864c3a768b690a3298e3ecd705c

[Fix]: bug with EOF introduced in 0d2e836

src/input/input_get_line.c
@@ -11,7 +11,7 @@ static int	input_arg_get_line(t_string *input)
 
 	arg = opt_get_command_line();
 	assert(arg != NULL);
-	if (arg[index] == '\0' && ft_strlen(arg) == 0)
+	if (arg[index] == '\0')
 		return (E_INPUT_EOF);
 	offset = ft_strchrpos(arg + index, '\n');
 	if (offset == -1)