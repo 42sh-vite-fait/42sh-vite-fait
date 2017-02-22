85210c65f7ec65bdc39f3f8466e17740cc44774c
Tom Mielczarek <tmielcza@student.42.fr>
2017-02-22 10:37:05 +0100

    [Fix + Refacto] add looping on commands even in non-interactive mode and merge interactive and non-interactive loops

--- src/shell/shell_loop.c
+++ src/shell/shell_loop.c
@@ -14,57 +14,46 @@ static void	shell_struct_clear(t_string *input, t_array *tokens, t_parser *parse
 	parser_clear(parser);
 }
 
-static int	interactive(t_string *input, t_array *tokens, t_parser *parser)
+static int	get_input(t_string *input, char const *prompt)
+{
+	int	status;
+	if (opt_is_set(OPT_INTERACTIVE))
+	{
+		while (1)
+		{
+			status = shell_input(input, prompt);
+			if (status != E_INPUT_ERROR)
+				break ;
+			history_add(fatal_malloc(string_create_dup(input->str)));
+			if (input->len == 0)
+				continue;
+		}
+	}
+	else
+		status = shell_input(input, prompt);
+	return (status);
+}
+
+static int	loop_on_command_lines(t_string *input, t_array *tokens, t_parser *parser)
 {
 	int	input_status;
 
 	input_status = E_INPUT_OK;
-	while (input_status == E_INPUT_OK)
+	while (1)
 	{
 		shell_struct_clear(input, tokens, parser);
-
-		if ((input_status = shell_input(input, SHELL_PS1)) == E_INPUT_ERROR)
+		input_status = get_input(input, SHELL_PS1);
+		if (input_status == E_INPUT_ERROR)
 			return (EXIT_FAILURE);
 		if (input_status == E_INPUT_EOF)
 			return (EXIT_SUCCESS);
-		if (input->len == 0)
-			continue ;
-
-		history_add(fatal_malloc(string_create_dup(input->str)));
-
 		if (shell_lexer(input, tokens) == LEXER_ERROR)
-			continue ;
-
+			return (EXIT_FAILURE);
 		if (shell_parser(input, parser, tokens) != PARSER_NO_ERROR)
-			continue ;
-
+			return (EXIT_FAILURE);
 		/* if (shell_exec(parser->ast) != EXEC_NO_ERROR) */
-		/* 	continue ; */
+		/*	return (EXIT_FAILURE); */
 	}
-	return (EXIT_SUCCESS);
-}
-
-static int	non_interactive(t_string *input, t_array *tokens, t_parser *parser)
-{
-	int	input_status;
-
-	input_status = shell_input(input, SHELL_PS1);
-	if (input_status == E_INPUT_ERROR)
-		return (EXIT_FAILURE);
-	if (input_status == E_INPUT_EOF)
-		return (EXIT_SUCCESS);
-
-	if (shell_lexer(input, tokens) == LEXER_ERROR)
-		return (EXIT_FAILURE);
-
-	if (shell_parser(input, parser, tokens) != PARSER_NO_ERROR)
-		return (EXIT_FAILURE);
-
-	/* if (shell_exec(parser->ast) != EXEC_NO_ERROR) */
-		/* return (EXIT_FAILURE); */
-	
-	string_shutdown(input);
-	return (EXIT_SUCCESS);
 }
 
 int 		shell_loop(void)
@@ -77,10 +66,8 @@ int 		shell_loop(void)
 	fatal_malloc(string_init(&input));
 	fatal_malloc(array_init(&tokens, sizeof(t_token)));
 	fatal_malloc(parser_init(&parser));
-	if (opt_is_set(OPT_INTERACTIVE))
-		ret = interactive(&input, &tokens, &parser);
-	else
-		ret = non_interactive(&input, &tokens, &parser);
+	ret = loop_on_command_lines(&input, &tokens, &parser);
+	string_shutdown(&input);
 	array_shutdown(&tokens);
 	parser_shutdown(&parser);
 	return (ret);
