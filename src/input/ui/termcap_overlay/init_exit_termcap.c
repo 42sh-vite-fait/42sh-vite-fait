f99f5249d17539064012dca1a119221150b60e63
Darius Kramer <dev@dkramer.fastmail.com>
2017-02-22 14:48:28 +0100

    [Termcaps]: fix infinite loop on linux caused by tcsetattr

--- src/input/ui/termcap_overlay/init_exit_termcap.c
+++ src/input/ui/termcap_overlay/init_exit_termcap.c
@@ -1,29 +1,13 @@
-#include <errno.h>
 #include "terminal.h"
 
 static struct termios	g_termios_backup;
 
-static int	terminal_set_termios(const struct termios new)
+static int	terminal_set_termios(const struct termios *new)
 {
-	struct termios	current;
-
-	if (tcgetattr(STDIN_FILENO, &current) < 0)
+	if (tcsetattr(STDIN_FILENO, TCSADRAIN, new) < 0)
 	{
-		error_set_context("tcgetattr: %s", strerror(errno));
+		error_set_context("tcsetattr: %s", strerror(errno));
 		return (ERR_TERM);
-	}
-	while (ft_memcmp(&new, &current, sizeof(current)) != 0)
-	{
-		if (tcsetattr(STDIN_FILENO, TCSADRAIN, &new) < 0)
-		{
-			error_set_context("tcsetattr: %s", strerror(errno));
-			return (ERR_TERM);
-		}
-		if (tcgetattr(STDIN_FILENO, &current) < 0)
-		{
-			error_set_context("tcgetattr: %s", strerror(errno));
-			return (ERR_TERM);
-		}
 	}
 	return (NO_ERROR);
 }
@@ -31,14 +15,13 @@ static int	terminal_set_termios(const struct termios new)
 int terminal_start_raw_mode(void)
 {
 	struct termios	raw;
-	if (isatty(1) == 0)
-		return (ERR_TERM);
+
 	raw = g_termios_backup;
 	raw.c_iflag &= ~(unsigned)IXON;
 	raw.c_lflag &= ~(unsigned)(ICANON | ECHO | IEXTEN | ISIG);
 	raw.c_cc[VMIN] = 1;
 	raw.c_cc[VTIME] = 0;
-	if (terminal_set_termios(raw) != NO_ERROR)
+	if (terminal_set_termios(&raw) != NO_ERROR)
 		return (ERR_TERM);
 	term_command("vi");
 	term_command("bw");
@@ -48,7 +31,7 @@ int terminal_start_raw_mode(void)
 int terminal_stop_raw_mode(void)
 {
 	term_command("ve");
-	if (terminal_set_termios(g_termios_backup) != NO_ERROR)
+	if (terminal_set_termios(&g_termios_backup) != NO_ERROR)
 		return (ERR_TERM);
 	return (NO_ERROR);
 }
@@ -57,8 +40,6 @@ int init_terminal_module(void)
 {
 	char	*name_term;
 
-	if (!isatty(STDIN_FILENO))
-		return (NO_ERROR);
 	if ((name_term = getenv("TERM")) == NULL)
 	{
 		error_set_context("TERM variable is missing");
