d7ad0fc557e6b279cb5ee2b101b8e50392ef73ec
Tom M <tmielcza@student.42.fr>
2017-02-24 18:01:49 +0100

    [Loop]: refacto shell loop (wip)

--- src/shell/shell_loop.c
+++ src/shell/shell_loop.c
@@ -7,175 +7,104 @@
 #include "input.h"
 #include "ft_printf.h"
 
-int shell_loop(void)
+#define EOF_ 0
+#define ERROR_ -1
+#define OK_ 1
+
+int	shell_lex(t_string *input, t_lexer *lexer, t_array *tokens)
 {
-	t_string	input_;
-	t_array		tokens_;
-	t_parser	parser_;
-	t_lexer		lexer_;
-	t_string	*input;
-	t_array		*tokens;
-	t_parser	*parser;
-	t_lexer		*lexer;
-	int			ret;
+	int			lexer_status;
+	int			input_status;
 	t_string	line;
 
-	fatal_malloc(string_init(&input_));
-	fatal_malloc(array_init(&tokens_, sizeof(t_token)));
-	fatal_malloc(parser_init(&parser_));
-	fatal_malloc(lexer_init(&lexer_));
+	lexer_status = LEXER_INPUT_INCOMPLETE;
+	string_init(&line);
+	while (lexer_status == LEXER_INPUT_INCOMPLETE)
+	{
+		string_truncate(&line, 0);
+		input_status = shell_input(&line, INPUT_REQUEST);
+		if (input_status != E_INPUT_OK)
+			return (input_status) ;
+		lexer_status = lexer_lex(lexer, tokens, &line);
+		if (lexer_status != LEXER_ERROR)
+			string_append(input, &line);
+	}
+	if (lexer_status == LEXER_ERROR)
+		error_print("lexer");
+	string_shutdown(&line);
+	return (OK_);
+}
 
-	fatal_malloc(string_init(&line));
-
-	input = &input_;
-	tokens = &tokens_;
-	parser = &parser_;
-	lexer = &lexer_;
-
-	int	input_status;
+int shell_parse(t_string *input, t_lexer *lexer, t_array *tokens,
+				t_parser *parser)
+{
 	int	lexer_status;
-	const char	*prompt;
+	int	parser_status;
 
-	input_status = E_INPUT_OK;
-	if (opt_is_set(OPT_INTERACTIVE))
+	lexer_status = shell_lex(input, lexer, tokens);
+	if (lexer_status != OK_)
+		return (lexer_status);
+	categorize_tokens(input, tokens);
+	lexer_clear_tokens(tokens);
+	parser_init_with_tokens(input, parser, tokens);
+	parser_status = parser_parse(parser);
+	if (parser_status != PARSER_NO_ERROR)
+		error_print("parser");
+	return (OK_);
+}
+
+int shell_loop(void)
+{
+	t_string	input;
+	t_array		tokens;
+	t_parser	parser;
+	t_lexer		lexer;
+	int			ret;
+
+	fatal_malloc(string_init(&input));
+	fatal_malloc(array_init(&tokens, sizeof(t_token)));
+	fatal_malloc(parser_init(&parser));
+	fatal_malloc(lexer_init(&lexer));
+
+	while (1)
 	{
-		while (1)
+		string_truncate(&input, 0);
+		string_shrink_to_fit(&input);
+		array_clear(&tokens);
+		parser_clear(&parser);
+
+		ret = shell_parse(&input, &lexer, &tokens, &parser);
+		if (ret == OK_)
 		{
-			string_truncate(input, 0);
-			string_truncate(&line, 0);
-			string_shrink_to_fit(input);
-			array_clear(tokens);
-			parser_clear(parser);
-
-			// While input is incomplete
-			lexer_status = LEXER_INPUT_INCOMPLETE;
-			prompt = SHELL_PS1;
-			while (1)
-			{
-				input_status = input_get_line(&line, prompt);
-				prompt = SHELL_PS2;
-				if (input_status == E_INPUT_ERROR) // Quit on error
-					exit(1);
-				if (input_status == E_INPUT_EOF) // No more input
-				{
-					if (!assert_stack_is_empty(lexer)) // Unfinished lexing
-						error_print("lexer");
-					exit(0);
-				}
-//				if (line.len == 0) // If the input is empty, loop
-//					continue ;
-				if (remove_trailing_escaped_newline(&line) != LINE_COMPLETE)
-					continue ;
-				lexer_status = lexer_lex(lexer, tokens, &line);
-				if (lexer_status != LEXER_ERROR)
-				{
-					string_append(input, &line);
-					string_truncate(&line, 0);
-					// TODO: cut lines multiple lines before pushing in history
-					history_add(fatal_malloc(string_create_dup(input->str)));
-				}
-				if (lexer_status != LEXER_INPUT_INCOMPLETE)
-					break ;
-			}
+			// TODO: cut lines multiple lines before pushing in history
+			if (opt_is_set(OPT_INTERACTIVE))
+				history_add(fatal_malloc(string_create_dup(input.str)));
 			if (opt_is_set(OPT_DEBUG_INPUT))
-				ft_printf("INPUT: [%s]\n", input->str);
-			if (lexer_status == LEXER_ERROR)
-			{
-				error_print("lexer");
-				continue ;
-			}
-			else
-			{
-				categorize_tokens(input, tokens);
-				lexer_clear_tokens(tokens);
-				if (opt_is_set(OPT_DEBUG_LEXER))
-					lexer_debug_print_tokens(input, tokens);
-			}
-			parser_init_with_tokens(input, parser, tokens);
-			ret = parser_parse(parser);
-			if (ret != PARSER_NO_ERROR)
-			{
-				error_print("parser");
-				continue ;
-			}
-			else if (opt_is_set(OPT_DEBUG_AST))
-				ast_debug_print(&parser->ast, input->str);
-		}
-		/* if (shell_exec(parser->ast) != EXEC_NO_ERROR) */
-		/*	return (EXIT_FAILURE); */
-	}
-
-
-
-	else if (!opt_is_set(OPT_CMD_STRING))
-	{
-		while (1)
-		{
-			string_truncate(input, 0);
-			string_truncate(&line, 0);
-			string_shrink_to_fit(input);
-			array_clear(tokens);
-			parser_clear(parser);
-
-			// While input is incomplete
-			lexer_status = LEXER_INPUT_INCOMPLETE;
-			while (1)
-			{
-				// TODO: replace with input_notty_get_line
-				input_status = input_get_line(&line, "");
-				if (input_status == E_INPUT_ERROR) // Quit on error
-					exit(1);
-				if (input_status == E_INPUT_EOF) // No more input
-				{
-					if (!assert_stack_is_empty(lexer)) // Unfinished lexing
-						error_print("lexer");
-					exit(0);
-				}
-//				if (line.len == 0) // If the input is empty, loop
-//					continue ;
-				if (remove_trailing_escaped_newline(&line) != LINE_COMPLETE)
-					continue ;
-				lexer_status = lexer_lex(lexer, tokens, &line);
-				if (lexer_status != LEXER_ERROR)
-				{
-					string_append(input, &line);
-					string_truncate(&line, 0);
-				}
-				if (lexer_status != LEXER_INPUT_INCOMPLETE)
-					break ;
-			}
-			if (opt_is_set(OPT_DEBUG_INPUT))
-				ft_printf("INPUT: [%s]\n", input->str);
-			if (lexer_status == LEXER_ERROR)
-			{
-				error_print("lexer");
-				break ;
-			}
-			categorize_tokens(input, tokens);
-			lexer_clear_tokens(tokens);
+				ft_printf("INPUT: [%s]\n", input.str); // A mettre plus loin
 			if (opt_is_set(OPT_DEBUG_LEXER))
-				lexer_debug_print_tokens(input, tokens);
-			// ???
-			parser_init_with_tokens(input, parser, tokens);
-			ret = parser_parse(parser);
-			if (ret != PARSER_NO_ERROR)
-			{
-				error_print("parser");
-				break ;
-			}
-			else if (opt_is_set(OPT_DEBUG_AST))
-				ast_debug_print(&parser->ast, input->str);
+				lexer_debug_print_tokens(&input, &tokens);
+			if (opt_is_set(OPT_DEBUG_AST))
+				ast_debug_print(&parser.ast, input.str);
+		}
+		else if (ret == EOF_)
+		{
+			// Faire des trucs avant de quitter, genre historique tout ca.
+			if (!assert_stack_is_empty(&lexer)) // Unfinished lexing
+				error_print("lexer");
+			exit(0);
+		}
+		else if (ret == ERROR_)
+		{
+			// Faire des trucs avant de quitter, genre historique tout ca.
+			exit(1);
 		}
 		/* if (shell_exec(parser->ast) != EXEC_NO_ERROR) */
 		/*	return (EXIT_FAILURE); */
 	}
 
-
-	
-	ret = 0;
-
-	string_shutdown(&input_);
-	array_shutdown(&tokens_);
-	parser_shutdown(&parser_);
+	// Faire ca avant de quitter
+	string_shutdown(&input);
+	array_shutdown(&tokens);
+	parser_shutdown(&parser);
 	return (ret);
 }
