d3788fc6a32e11672dc45eab4df73c55676ebcfb
Darius Kramer <dev@dkramer.fastmail.com>
2017-02-25 15:39:08 +0100

    [Exec]: replace recursive AND_OR with stack-loop, avoid possible stack-overflow

--- src/exec/and_or/exec_node_and_or.c
+++ src/exec/and_or/exec_node_and_or.c
@@ -1,64 +1,36 @@
 #include <assert.h>
 #include "exec.h"
 
-#include <stdio.h>
 extern t_tree_walker	*const g_walkers[];
 
-int		exec_node_and_or(const t_ast_node *node)
+static bool	should_we_execute_next_child(int token_type, int prev_ret)
 {
-	int		ret;
-
-	assert(node != NULL);
-	ret = g_walkers[node->left->type](node->left);
-	if (node->token->type == E_TOKEN_AND_IF)
-	{
-		if (ret == 0)
-			return (g_walkers[node->right->type](node->right));
-	}
-	else if (node->token->type == E_TOKEN_OR_IF)
-	{
-		if (ret != 0)
-			return (g_walkers[node->right->type](node->right));
-	}
-	else
-		assert(0);
-	return (ret);
+	if (token_type == E_TOKEN_AND_IF && prev_ret == 0)
+		return (true);
+	else if (token_type == E_TOKEN_OR_IF && prev_ret != 0)
+		return (true);
+	return (false);
 }
 
-/* static int	conditionnal_exec_and_or(const t_ast_node *node, int prev_ret) */
-/* { */
-/* 	if (node->token->type == E_TOKEN_AND_IF) */
-/* 	{ */
-/* 		if (prev_ret == 0) */
-/* 			return (g_walkers[node->right->type](node->right)); */
-/* 	} */
-/* 	else */
-/* 	{ */
-/* 		if (prev_ret != 0) */
-/* 			return (g_walkers[node->right->type](node->right)); */
-/* 	} */
-/* 	return (prev_ret); */
-/* } */
+int			exec_node_and_or(const t_ast_node *node)
+{
+	t_array		andor_children_stack;
+	t_array		andor_token_type_stack;
+	t_ast_node	*child;
+	int			token_type;
+	int			exit_status;
 
-/* int		exec_node_and_or(const t_ast_node *node) */
-/* { */
-/* 	t_array	andor_nodes_stack; */
-/* 	int		ret; */
-
-/* 	assert(node != NULL); */
-/* 	andor_nodes_stack = gather_nodes(node, E_AST_AND_OR); */
-/* 	array_pop(&andor_nodes_stack, &node); */
-/* 	// Crash: la stack contient les nodes des commandes/pipes, pas les AND_OR */
-/* 	// Cette facon fonctionne pour les LIST */
-/* 	// Supprimer gather_nodes et utiliser une recursion ? */
-/* 	// Ou créer une structure qui contient le node left/right et le type du token */
-/* 	ret = g_walkers[node->type](node); */
-/* 	array_pop(&andor_nodes_stack, &node); */
-/* 	ret = conditionnal_exec_and_or(node, ret); */
-/* 	while (andor_nodes_stack.len > 0) */
-/* 	{ */
-/* 		array_pop(&andor_nodes_stack, &node); */
-/* 		ret = conditionnal_exec_and_or(node, ret); */
-/* 	} */
-/* 	return (ret); */
-/* } */
+	assert(node != NULL);
+	andor_children_stack = gather_childrens_nodes(node, E_AST_AND_OR);
+	andor_token_type_stack = gather_nodes_token_type(node, E_AST_AND_OR);
+	array_pop(&andor_children_stack, &child);
+	exit_status = g_walkers[child->type](child);
+	while (andor_token_type_stack.len > 0)
+	{
+		array_pop(&andor_token_type_stack, &token_type);
+		array_pop(&andor_children_stack, &child);
+		if (should_we_execute_next_child(token_type, exit_status))
+			exit_status = g_walkers[child->type](child);
+	}
+	return (exit_status);
+}
