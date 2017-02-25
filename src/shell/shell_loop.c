23a52647517dabfcd1c57e38b5d9df4ab80bb970
Tom M <tmielcza@student.42.fr>
2017-02-25 15:53:09 +0100

    [Norme]: split shell_loop

--- src/shell/shell_loop.c
+++ src/shell/shell_loop.c
@@ -62,67 +62,64 @@ int shell_parse(t_string *input, t_lexer *lexer, t_array *tokens,
 	return (OK_);
 }
 
+int	shell_loop2(t_string *input, t_array *tokens, t_parser *parser, t_lexer *lexer)
+{
+	int	input_parsing_status;
+
+	while (1)
+	{
+		string_truncate(input, 0);
+		string_shrink_to_fit(input);
+		array_clear(tokens);
+		parser_clear(parser);
+		input_parsing_status = shell_parse(input, lexer, tokens, parser);
+		if (input_parsing_status == OK_)
+		{
+			// TODO: cut lines multiple lines before pushing in history
+			if (opt_is_set(OPT_INTERACTIVE))
+				history_add(fatal_malloc(string_create_dup(input->str)));
+			if (opt_is_set(OPT_DEBUG_INPUT))
+				ft_printf("INPUT: [%s]\n", input->str);
+			if (opt_is_set(OPT_DEBUG_LEXER))
+				lexer_debug_print_tokens(input, tokens);
+			if (opt_is_set(OPT_DEBUG_AST))
+				ast_debug_print(&parser->ast, input->str);
+		}
+		else if (input_parsing_status == EOF_)
+		{
+			if (!assert_stack_is_empty(lexer))
+				error_print("lexer");
+			return (0);
+		}
+		else if (input_parsing_status == INVALID_)
+		{
+			if (!opt_is_set(OPT_INTERACTIVE))
+				return (1);
+		}
+		else if (input_parsing_status == ERROR_)
+			return (1);
+		/* if (shell_exec(parser->ast) != EXEC_NO_ERROR) */
+		/*	return (EXIT_FAILURE); */
+	}
+}
+
 int shell_loop(void)
 {
 	t_string	input;
 	t_array		tokens;
 	t_parser	parser;
 	t_lexer		lexer;
-	int			input_parsing_status;
-	int			exit_status;
+	int			ret;
 
 	fatal_malloc(string_init(&input));
 	fatal_malloc(array_init(&tokens, sizeof(t_token)));
 	fatal_malloc(parser_init(&parser));
 	fatal_malloc(lexer_init(&lexer));
-
-	exit_status = 0;
-	while (1)
-	{
-		string_truncate(&input, 0);
-		string_shrink_to_fit(&input);
-		array_clear(&tokens);
-		parser_clear(&parser);
-
-		input_parsing_status = shell_parse(&input, &lexer, &tokens, &parser);
-		if (input_parsing_status == OK_)
-		{
-			// TODO: cut lines multiple lines before pushing in history
-			if (opt_is_set(OPT_INTERACTIVE))
-				history_add(fatal_malloc(string_create_dup(input.str)));
-			if (opt_is_set(OPT_DEBUG_INPUT))
-				ft_printf("INPUT: [%s]\n", input.str);
-			if (opt_is_set(OPT_DEBUG_LEXER))
-				lexer_debug_print_tokens(&input, &tokens);
-			if (opt_is_set(OPT_DEBUG_AST))
-				ast_debug_print(&parser.ast, input.str);
-		}
-		else if (input_parsing_status == EOF_)
-		{
-			if (!assert_stack_is_empty(&lexer)) // Unfinished lexing
-				error_print("lexer");
-			break ;
-		}
-		else if (input_parsing_status == INVALID_)
-		{
-			if (!opt_is_set(OPT_INTERACTIVE))
-			{
-				exit_status = 1;
-				break ;
-			}
-		}
-		else if (input_parsing_status == ERROR_)
-		{
-			exit_status = 1;
-			break ;
-		}
-		/* if (shell_exec(parser->ast) != EXEC_NO_ERROR) */
-		/*	return (EXIT_FAILURE); */
-	}
-
+	ret = shell_loop2(&input, &tokens, &parser, &lexer);
 	// Faire des trucs avant de quitter, genre historique tout ca.
 	string_shutdown(&input);
 	array_shutdown(&tokens);
+	lexer_shutdown(&lexer);
 	parser_shutdown(&parser);
-	return (input_parsing_status);
+	return (ret);
 }
