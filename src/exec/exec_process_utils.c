a1eccef7aec8efcfe3ecc638c46306e5bd16c6fd
Darius Kramer <dev@dkramer.fastmail.com>
2017-03-03 11:30:52 +0100

    [Exec]: add support for exit status

--- src/exec/exec_process_utils.c
+++ src/exec/exec_process_utils.c
@@ -3,12 +3,48 @@
 #include <string.h>
 #include "errors.h"
 #include "exec.h"
+#include "ft_printf.h"
 
-static int get_exit_status(int status) // TODO
+static const char	*g_sig_to_str[] = {
+     [SIGHUP] = "Hangup",
+     [SIGQUIT] = "Quit",
+     [SIGILL] = "Illegal instruction",
+     [SIGTRAP] = "Trace/BPT trap",
+	 [SIGABRT] = "Abort trap",
+     [SIGEMT] = "EMT trap",
+     [SIGFPE] = "Floating point exception",
+     [SIGKILL] = "Killed",
+     [SIGBUS] = "Bus error",
+     [SIGSEGV] = "Segmentation fault",
+     [SIGSYS] = "Bad system call",
+     [SIGALRM] = "Alarm clock",
+     [SIGTERM] = "Terminated",
+     [SIGSTOP] = "Suspended",
+     [SIGTSTP] = "Terminal suspended",
+     [SIGTTIN] = "Stopped (tty input)",
+     [SIGTTOU] = "Stopped (tty ouput)",
+     [SIGXCPU] = "Cputime limit exceeded",
+     [SIGXFSZ] = "Filesize limit exceeded",
+     [SIGVTALRM] = "Virtual timer expired",
+     [SIGPROF] = "Profiler timer epxired",
+     [SIGUSR1] = "User defined signal 1",
+     [SIGUSR2] = "User defined signal 2",
+};
+
+static int get_exit_status(int status)
 {
+	int	sig;
+
 	if (WIFEXITED(status))
 		return (WEXITSTATUS(status));
-	return (0);
+	else if (WIFSIGNALED(status))
+	{
+		sig = WTERMSIG(status);
+		if (g_sig_to_str[sig])
+			ft_printf("%d, %s\n", sig, g_sig_to_str[sig]);
+		return (sig + 128);
+	}
+	return (-1);
 }
 
 int	wait_child_process_group(pid_t last_pid, pid_t pgid)
