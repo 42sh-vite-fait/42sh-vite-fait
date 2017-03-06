c2b7d86613055a5a031381374b36b4574f8e5588
Darius Kramer <dev@dkramer.fastmail.com>
2017-03-06 18:55:42 +0100

    [Test]: avoid use of /dev/random, because determinism

--- test/yyang_bash_tests/pipe/cat_base64_head/input.sh
+++ test/yyang_bash_tests/pipe/cat_base64_head/input.sh
@@ -1,1 +1,9 @@
-cat /dev/random|base64|head -c 42
+echo "===== 1"
+python -c 'print "A"*100000' > /tmp/pip_bareme_spec_file
+echo "===== 2"
+cat /tmp/pip_bareme_spec_file | base64|head -c 2 | wc
+echo "===== 3"
+python -c 'print "A"*200000' > /tmp/pip_bareme_spec_file
+echo "===== 4"
+cat /tmp/pip_bareme_spec_file | base64|head -c 2 | wc
+echo "===== 5"
