658051a4c0fe02bd58da2910c0de7fee4e58a64e
Tom M <tmielcza@student.42.fr>
2017-03-06 18:28:53 +0100

    [History]: replace get_next_line w/ str_token in history_add

--- src/history/history_add.c
+++ src/history/history_add.c
@@ -3,31 +3,9 @@
 #include "cbuffer_42.h"
 #include "string_42.h"
 #include "history.h"
+#include "misc.h"
 
 extern t_history		g_history;
-
-static const char	*get_next_line(t_string *next_line, const char *input)
-{
-	ssize_t	len;
-
-	len = ft_strchrpos(input, '\n');
-	if (len == 0)
-		return (NULL);
-	if (len > 0)
-	{
-		fatal_malloc(string_init_ndup(next_line, input, len));
-		input += len + 1;
-	}
-	else
-	{
-		len = ft_strlen(input);
-		if (len == 0)
-			return (NULL);
-		fatal_malloc(string_init_ndup(next_line, input, len));
-		input += len;
-	}
-	return (input);
-}
 
 size_t		history_add(t_string *command)
 {
@@ -36,7 +14,7 @@ size_t		history_add(t_string *command)
 
 	assert(command != NULL);
 	command_str = command->str;
-	while ((command_str = get_next_line(&line, command_str)) != NULL)
+	while ((command_str = str_token(&line, command_str, '\n')) != NULL)
 	{
 		g_history.last_id += 1;
 		cbuffer_push_back(&g_history.commands, &line);
