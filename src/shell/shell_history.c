f214ef9ef513182401103ae13647eb78226e5357
Tom M <tmielcza@student.42.fr>
2017-03-08 15:41:24 +0100

    [Fix]: compile

--- src/shell/shell_history.c
+++ src/shell/shell_history.c
@@ -1,11 +1,11 @@
-#include "ctypes.h"
+#include "ctype_42.h"
 #include "history.h"
 #include "var.h"
 
 #define HIST_DEFAULT_SIZE 128
 #define HIST_DEFAULT_FILE ".sh_history"
 
-static size_t	atou(const char *size_str)
+static size_t	atou(const char *str)
 {
 	size_t		ret;
 
@@ -27,7 +27,7 @@ void			shell_history_init(void)
 	size_t		hist_size;
 	const char	*hist_file;
 	const char	*home;
-	t_string	*default_hist_file;
+	t_string	default_hist_file;
 
 	status = var_get("HISTSIZE", &hist_size_str);
 	if (status == NO_ERROR && hist_size_str != NULL)
@@ -40,18 +40,16 @@ void			shell_history_init(void)
 		hist_size = HIST_DEFAULT_SIZE;
 	history_init(hist_size); // fatal_malloc in history_init
 
-	status = var_get("HISTFILE", hist_file);
+	status = var_get("HISTFILE", &hist_file);
 	if (status == NO_ERROR && hist_file != NULL
 		&& history_load_from_file(hist_file) == 0)
 		return ;
 
-	status = var_get("HOME", home);
+	status = var_get("HOME", &home);
 	if (status == NO_ERROR && home != NULL)
 	{
 		string_init_dup(&default_hist_file, home); // fatal_malloc
 		string_cat(&default_hist_file, HIST_DEFAULT_FILE); // fatal_malloc
 		history_load_from_file(home);
 	}
-
-	history_load_from_file();
 }
