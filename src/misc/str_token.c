ec58e21955a92ab2585b3cc256f851fa4c2325eb
Tom M <tmielcza@student.42.fr>
2017-03-08 21:41:44 +0100

    [Fix]: fix in str_token : an empty token does not mean an end of input

--- src/misc/str_token.c
+++ src/misc/str_token.c
@@ -7,9 +7,7 @@ const char	*str_token(t_string *next_line, const char *input, char sep)
 	ssize_t	len;
 
 	len = ft_strchrpos(input, sep);
-	if (len == 0)
-		return (NULL);
-	if (len > 0)
+	if (len >= 0)
 	{
 		fatal_malloc(string_init_ndup(next_line, input, len));
 		input += len + 1;
