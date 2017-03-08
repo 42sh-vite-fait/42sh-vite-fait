983d662867d716d16cf559a37e22b07a6a8f3bdb
Tom M <tmielcza@student.42.fr>
2017-03-08 23:35:17 +0100

    [Fix]: fix history loading
    * remove useless includes / defines
    * prevent reading from HISTFILE if not a regular file or too big
    * make history_load_from_file() return -1 only if open() fails
    * change inject_commands() signature

--- src/history/history_load.c
+++ src/history/history_load.c
@@ -1,40 +1,37 @@
 #include <unistd.h>
 #include <fcntl.h>
-#include "typedefs_42.h"
+#include <sys/stat.h>
 #include "string_42.h"
-#include "memory.h"
 #include "history.h"
 #include "misc.h"
 
+#define HIST_MAX_SIZE 0x1000000
+
 extern t_history	g_history;
 
-static int			inject_commands(const char *file)
+static void			inject_commands(const char *file)
 {
 	t_string	command;
 
 	while ((file = str_token(&command, file, '\n')) != NULL)
 		history_add(&command);
-	return (0);
 }
 
 int					history_load_from_file(const char *path)
 {
 	t_string	file;
 	int			fd;
+	struct stat	stat;
 
+	string_init(&file);
 	if ((fd = open(path, O_RDONLY | O_CLOEXEC)) == -1)
 		return (-1);
-	if (string_read_from_fd(&file, fd) == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
+	if (fstat(fd, &stat) == 0 &&
+		S_ISREG(stat.st_mode) &&
+		stat.st_size <= HIST_MAX_SIZE &&
+		string_read_from_fd(&file, fd) != NULL)
+		inject_commands(file.str);
 	close(fd);
-	if (inject_commands(file.str) == -1)
-	{
-		string_shutdown(&file);
-		return (-1);
-	}
 	string_shutdown(&file);
 	return (0);
 }
