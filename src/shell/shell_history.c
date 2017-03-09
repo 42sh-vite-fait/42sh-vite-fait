7b4b90b3abd678f851b16ab863d94dda703d1d12
Tom M <tmielcza@student.42.fr>
2017-03-09 16:43:05 +0100

    [History]: refacto

--- src/shell/shell_history.c
+++ src/shell/shell_history.c
@@ -18,14 +18,32 @@ static size_t	atou(const char *str)
 	return (ret);
 }
 
+static void		history_file_action(int (*action)(const char*))
+{
+	const char	*hist_file;
+	const char	*home;
+	t_string	default_hist_file;
+	int			status;
+
+	status = var_get("HISTFILE", &hist_file);
+	if (status == NO_ERROR && hist_file != NULL
+		&& action(hist_file) == 0)
+		return ;
+
+	status = var_get("HOME", &home);
+	if (status == NO_ERROR && home != NULL)
+	{
+		fatal_malloc(string_init_dup(&default_hist_file, home));
+		fatal_malloc(string_cat(&default_hist_file, HIST_DEFAULT_FILE));
+		action(default_hist_file.str);
+	}
+}
+
 void			shell_history_init(void)
 {
 	const char	*hist_size_str;
 	int			status;
 	size_t		hist_size;
-	const char	*hist_file;
-	const char	*home;
-	t_string	default_hist_file;
 
 	status = var_get("HISTSIZE", &hist_size_str);
 	if (status == NO_ERROR && hist_size_str != NULL)
@@ -36,38 +54,11 @@ void			shell_history_init(void)
 	}
 	else
 		hist_size = HIST_DEFAULT_SIZE;
-	history_init(hist_size); // fatal_malloc in history_init
-
-	status = var_get("HISTFILE", &hist_file);
-	if (status == NO_ERROR && hist_file != NULL
-		&& history_load_from_file(hist_file) == 0)
-		return ;
-
-	status = var_get("HOME", &home);
-	if (status == NO_ERROR && home != NULL)
-	{
-		string_init_dup(&default_hist_file, home); // fatal_malloc
-		string_cat(&default_hist_file, HIST_DEFAULT_FILE); // fatal_malloc
-		history_load_from_file(default_hist_file.str);
-	}
+	history_init(hist_size);
+	history_file_action(history_load_from_file);
 }
 
 void			shell_history_shutdown(void)
 {
-	int			status;
-	const char	*hist_file;
-	const char	*home;
-	t_string	default_hist_file;
-
-	status = var_get("HISTFILE", &hist_file);
-	if (status == NO_ERROR && hist_file != NULL
-		&& history_save_into_file(hist_file) == 0)
-		return ;
-	status = var_get("HOME", &home);
-	if (status == NO_ERROR && home != NULL)
-	{
-		fatal_malloc(string_init_dup(&default_hist_file, home));
-		fatal_malloc(string_cat(&default_hist_file, HIST_DEFAULT_FILE));
-		history_save_into_file(default_hist_file.str);
-	}
+	history_file_action(history_save_into_file);
 }
