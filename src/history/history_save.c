036e9f363a76d9370c030a9fae7815aa4eec81ee
Tom M <tmielcza@student.42.fr>
2017-03-09 14:39:58 +0100

    [History]: improve history saving functions
    * Add malloc protection
    * Better string initialization/shutdown policy
    * history_save_into_file fails only if open fails

--- src/history/history_save.c
+++ src/history/history_save.c
@@ -9,10 +9,8 @@ extern t_history	g_history;
 
 static int	convert_and_write_to_file(t_string *command, size_t cmd_id, int fd)
 {
-	if (string_replace(command, history_get(cmd_id)->str) == NULL)
-		return (-1);
-	if (string_insert(command, command->len, "\n", 1) == NULL)
-		return (-1);
+	fatal_malloc(string_replace(command, history_get(cmd_id)->str));
+	fatal_malloc(string_insert(command, command->len, "\n", 1));
 	if (string_write_to_fd(command, fd) == -1)
 		return (-1);
 	return (0);
@@ -24,22 +22,17 @@ int			history_save_into_file(const char *path)
 	size_t		i;
 	int			fd;
 
-	if ((fd = open(path, O_CREAT | O_RDWR | O_TRUNC | O_CLOEXEC, 0600)) == -1)
+	if ((fd = open(path, O_CREAT | O_WRONLY | O_TRUNC | O_CLOEXEC, 0600)) == -1)
 		return (-1);
-	if (string_init(&command) == NULL)
-		return (-1);
+	fatal_malloc(string_init(&command));
 	i = g_history.last_id;
 	while (i > g_history.last_id - g_history.commands.len)
 	{
 		if (convert_and_write_to_file(&command, i, fd) == -1)
-		{
-			close(fd);
-			free(command.str);
-			return (-1);
-		}
+			break ;
 		--i;
 	}
 	close(fd);
-	free(command.str);
+	string_shutdown(&command);
 	return (0);
 }
