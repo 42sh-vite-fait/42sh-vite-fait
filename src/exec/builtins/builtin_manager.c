9ddd4e9c1b9346a8d5eac489ae0afde195982587
Tom M <tmielcza@student.42.fr>
2017-03-13 13:01:34 +0100

    [Builtins]: fix and simplify builtins utils

--- src/exec/builtins/builtin_manager.c
+++ src/exec/builtins/builtin_manager.c
@@ -7,9 +7,6 @@
 
 #define ARRAY_ELEMS(A)	(sizeof(A) / sizeof(A[0]))
 
-/*
-** These shall be sorted
-*/
 t_pair_name_builtin	g_builtins[] =
 {
 //	{"cd", NULL},
@@ -20,47 +17,22 @@ t_pair_name_builtin	g_builtins[] =
 //	{"pwd", NULL},
 //	{"setenv", NULL},
 };
-const size_t		g_builtins_nb = sizeof(g_builtins) / sizeof(g_builtins[0]);
 
-#ifndef NDEBUG
-
-static int	are_builtins_sorted(void)
-{
-	size_t	i;
-
-	i = 0;
-	while (i + 1 < ARRAY_ELEMS(g_builtins))
-	{
-		if (ft_strcmp(g_builtins[i].name, g_builtins[i + 1].name) > 0)
-			return (0);
-		i += 1;
-	}
-	return (1);
-}
-
-#endif
+const size_t		g_builtins_len = ARR_SIZ_MAX(g_builtins);
 
 static int	find_builtin_id(char const *name, size_t len)
 {
-	size_t	min;
-	size_t	max;
 	size_t	i;
 	int		diff;
 
-	min = 0;
-	max = ARRAY_ELEMS(g_builtins);
+	i = 0;
 	assert(name != NULL);
-	assert(are_builtins_sorted());
-	while (min != max)
+	while (i < g_builtins_len)
 	{
-		i = min + (max - min) / 2;
 		diff = ft_strncmp(name, g_builtins[i].name, len);
 		if (diff == 0 && g_builtins[i].name[len] == '\0')
 			return ((int)i);
-		else if (diff > 0)
-			min += (max - min + 1) / 2;
-		else
-			max -= (max - min + 1) / 2;
+		i += 1;
 	}
 	return (-1);
 }
@@ -75,7 +47,7 @@ int			exec_builtin(int ac, const char *const *av)
 	int		id;
 	int	exit_status;
 
-	id = find_builtin_id(av[0], ft_strlen(av[0]) + 1);
+	id = find_builtin_id(av[0], ft_strlen(av[0]));
 	assert(id != -1);
 	exit_status = g_builtins[id].builtin(ac, av);
 	exit_status_set_last(exit_status);
