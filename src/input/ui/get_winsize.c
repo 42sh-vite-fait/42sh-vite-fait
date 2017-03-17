00166457e33f3cc042e5ca3760764152dda446e9
Darius Kramer <dev@dkramer.fastmail.com>
2017-03-17 12:37:09 +0100

    [UI]: remove unwanted codes

--- src/input/ui/get_winsize.c
+++ src/input/ui/get_winsize.c
@@ -1,10 +1,16 @@
 #include "terminal.h"
+/*
+ * TODO
+ * Le fd d'ecriture doit être stdin pour garantir qu'il s'agit d'un terminal
+ */
 
 int			get_cols(void)
 {
 	struct winsize		ts;
+	int	ret;
 
-	ioctl(1, TIOCGWINSZ, &ts);
+	ret = ioctl(1, TIOCGWINSZ, &ts);
+	assert(ret != -1);
 	if (ts.ws_col <= 0)
 		return (10);
 	return (ts.ws_col);
--- src/input/ui/ui_loop/ui_prompt.c
+++
@@ -1,6 +0,0 @@
-#include "user_interface.h"
-
-size_t prompt(const char *prompt)
-{
-	return (ft_printf("%s", prompt));
-}
