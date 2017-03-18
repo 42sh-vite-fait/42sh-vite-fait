d85813ad8e56e611d321ecd34e8eaa1666740574
Darius Kramer <dev@dkramer.fastmail.com>
2017-03-18 12:50:14 +0100

    [Terminal]: forgot to add prototypes for winsize interface

--- inc/terminal.h
+++ inc/terminal.h
@@ -12,6 +12,8 @@ int				init_terminal_module(void);
 int				terminal_start_raw_mode(void);
 int				terminal_stop_raw_mode(void);
 struct winsize	terminal_get_winsize(void);
+unsigned		terminal_get_rows(void);
+unsigned		terminal_get_cols(void);
 void			terminal_move_cursor(const char *cap, unsigned len);
 void			terminal_execute_caps(const char *cap);
 int				terminal_check_caps(void);
