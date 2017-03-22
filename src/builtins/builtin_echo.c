4af3e29fd5a7f7a53b48a2c1831bf34e5ff256e6
Darius Kramer <dev@dkramer.fastmail.com>
2017-03-22 18:14:20 +0100

    [Builtin] fix bug if echo had no argument

--- src/builtins/builtin_echo.c
+++ src/builtins/builtin_echo.c
@@ -5,31 +5,36 @@
 #include "ft_printf.h"
 #include "builtins.h"
 
-int		builtin_echo(int argc, const char *const *argv)
+static void	concat_args(t_string *buf, const char *const *argv)
 {
-	t_string	buf;
-	bool		newline;
+	bool	newline;
 
-	(void)argc;
-	argv += 1;
 	newline = true;
 	if (ft_strcmp(*argv, "-n") == 0)
 	{
 		newline = false;
 		argv += 1;
 	}
-	fatal_malloc(string_init(&buf));
 	while (*argv)
 	{
-		fatal_malloc(string_cat(&buf, *argv));
+		fatal_malloc(string_cat(buf, *argv));
 		if (argv[1] != NULL)
-			fatal_malloc(string_ncat(&buf, " ", 1));
+			fatal_malloc(string_ncat(buf, " ", 1));
 		argv += 1;
 	}
 	if (newline)
-		ft_printf("%s\n", buf.str);
-	else
-		ft_printf("%s", buf.str);
+		fatal_malloc(string_ncat(buf, "\n", 1));
+}
+
+int			builtin_echo(int argc, const char *const *argv)
+{
+	t_string	buf;
+
+	fatal_malloc(string_init(&buf));
+	argv += 1;
+	if (argc > 1)
+		concat_args(&buf, argv);
+	ft_printf("%s", buf.str);
 	string_shutdown(&buf);
 	return (0);
 }
