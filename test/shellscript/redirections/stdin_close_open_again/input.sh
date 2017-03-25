813c4ac93b8acd45e6fa75f928e5b1244e9c4076
Darius Kramer <dev@dkramer.fastmail.com>
2017-03-25 20:17:42 +0100

    [Test] add more tests, pipe/redirections

---
+++ test/shellscript/redirections/stdin_close_open_again/input.sh
@@ -0,0 +1,10 @@
+echo 'abc' > /tmp/redir_stdin_close
+
+# with binary
+/bin/cat -e <&- </tmp/redir_stdin_close
+
+# with builtin
+echo 'def' <&- </tmp/redir_stdin_close
+
+echo end
+cat -e /tmp/redir_stdin_close
