1ac90b8a867c2484f1c70240710a23a7875b3fc3
Darius Kramer <dev@dkramer.fastmail.com>
2017-03-26 16:27:35 +0200

    [Test] add exec_new_path

--- test/shellscript/misc/exec_new_path/input.sh
+++ test/shellscript/misc/exec_new_path/input.sh
@@ -1,17 +1,23 @@
 mkdir -p /tmp/test_binary_path
 echo "echo abc" > /tmp/test_binary_path/myexec
 chmod 755 /tmp/test_binary_path/myexec
+
 echo "=======1"
 myexec
+
 echo "=======2"
-setenv PATH "/tmp/test_binary_path:/usr/bin:/bin"
+2>/dev/null export PATH="/tmp/test_binary_path:/usr/bin:/bin" || setenv PATH "/tmp/test_binary_path:/usr/bin:/bin"
 myexec
+
 echo "=======3"
 /tmp/test_binary_path/myexec
+
 echo "=======4"
 setenv PATH
 myexec > /dev/null
+
 echo "=======5"
 unsetenv PATH
 myexec > /dev/null
+
 echo "=======6"
