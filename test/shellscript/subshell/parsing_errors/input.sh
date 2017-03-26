c4156c378efd8a09f46e132520ce674ce5aa5501
Darius Kramer <dev@dkramer.fastmail.com>
2017-03-26 16:18:22 +0200

    [Test] fix subshell err parsing test

--- test/shellscript/subshell/parsing_errors/input.sh
+++ test/shellscript/subshell/parsing_errors/input.sh
@@ -1,10 +1,51 @@
-()
-(;)
-(|)
-(>)
-(&&)
-(echo a;)
-(echo b&&)
-(echo c|)
-(echo d ;())
-(;echo e)
+echo '()' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
+
+echo '(;)' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
+
+echo '(|)' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
+
+echo '(>)' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
+
+echo '(&&)' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
+
+echo '(echo a;)' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
+
+echo '(echo b &&)' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
+
+echo '(echo b && > /tmp/subshell_err_zut)' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
+
+echo '(echo c |)' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
+
+echo '(echo c | >> /tmp/subshell_err_zut)' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
+
+echo '(echo d; ())' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
+
+echo '(echo d; (echo d2;));' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
+
+echo '(;echo e)' > /tmp/subshell_err_parsing
+/tmp/bin/42sh < /tmp/subshell_err_parsing 2> /tmp/subshell_err_parsing_msg
+cat /tmp/subshell_err_parsing_msg
