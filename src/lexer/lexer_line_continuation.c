9aa5886c8ac4cf34f01cc4ce6f29e10198ce58fb
Tom M <tmielcza@student.42.fr>
2017-03-30 21:18:22 +0200

    [Quoting] hopefully fixes #15

--- src/lexer/lexer_line_continuation.c
+++ src/lexer/lexer_line_continuation.c
@@ -2,16 +2,31 @@
 #include "lexer.h"
 #include "string_42.h"
 #include "misc.h"
+#include "quoting.h"
 
 int			remove_trailing_escaped_newline(t_string *input)
 {
+	t_automaton	quoting;
+	size_t		i;
+
 	assert(input->str != NULL);
-	if (input->len >= 2
-		&& input->str[input->len - 1] == '\n'
-		&& is_escaped(input->str + input->len - 1, input->str))
+	i = 0;
+	quoting_automaton_init(&quoting);
+	while (i < input->len)
 	{
-		string_remove_back(input, 2);
-		return (LINE_INCOMPLETE);
+		if (input->str[i] == '\n' && (quoting.current_state == E_BSLASHED
+			|| quoting.current_state == E_DQUOTEDBSLASHED))
+		{
+			quoting_automaton_step(&quoting, input->str[i]);
+			string_remove(input, i - 1, 2);
+			i -= 1;
+			continue ;
+		}
+		quoting_automaton_step(&quoting, input->str[i]);
+		i += 1;
 	}
+	automaton_shutdown(&quoting);
+	if (input->len == 0 || input->str[input->len - 1] != '\n')
+		return (LINE_INCOMPLETE);
 	return (LINE_COMPLETE);
 }
