121172d8e174fd84db708752e5ab431020c722f0
Darius Kramer <dev@dkramer.fastmail.com>
2017-04-02 17:34:53 +0200

    [Test] add old debug test with debug option

--- test/shellscript/complete_command/list_subshell_redirs_debug_ast_NOLOAD/input.sh
+++
@@ -1,4 +0,0 @@
-echo 'useless commands'; (ls -1 . NO_DIR 2>/dev/null | head -n 1 | cat -e -- - NO_FILE) 2>&1 >/tmp/subshell_out_err || mkdir /tmp/useless_subshell_dir && rmdir /tmp/useless_subshell_dir; cat /tmp/subshell_out_err
-
-rm -rf /tmp/useless_subshell_dir
-rm -f /tmp/subshell_out_err
--- test/shellscript/subshell/subshell_ception_debug_ast_NOLOAD/input.sh
+++
@@ -1,1 +0,0 @@
-(echo a | (echo b1 || echo b2 | echo b3 | echo b4) ; (echo c1; (echo c11 && echo c12) && echo d))
