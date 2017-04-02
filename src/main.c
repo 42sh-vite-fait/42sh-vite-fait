c295b5764f60635ce30096ee2e19457768e25350
Darius Kramer <dev@dkramer.fastmail.com>
2017-04-02 17:42:28 +0200

    [Leaks] remove check_leaks function from main

--- src/main.c
+++ src/main.c
@@ -6,30 +6,13 @@
 /*   By: djean <djean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/01 20:22:21 by djean             #+#    #+#             */
-/*   Updated: 2017/04/01 20:22:22 by djean            ###   ########.fr       */
+/*   Updated: 2017/04/02 17:42:11 by djean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "init.h"
 #include "opt.h"
 #include "shell.h"
-#include "ft_printf.h"
-
-// TODO remove
-static void	check_leaks(void)
-{
-	char	*cmd;
-
-	ft_asprintf(&cmd, "leaks %ld >/dev/null", getpid());
-	if (system(cmd) != 0)
-	{
-		free(cmd);
-		ft_asprintf(&cmd, "leaks %ld", getpid());
-		system(cmd);
-		exit(-2);
-	}
-	free(cmd);
-}
 
 int		main(int argc, char *argv[])
 {
@@ -40,7 +23,5 @@ int		main(int argc, char *argv[])
 	ret = shell_loop();
 	if (opt_is_set(OPT_INTERACTIVE))
 		shell_history_shutdown();
-	if (getenv("CHECK_LEAKS"))
-		check_leaks();
 	return (ret);
 }
